b33dd911f93f39ebf9b8aa29174f9b1fa489b038
Fomin Alexey <fomin.alexey@outlook.com>
2023-10-29 21:51:55 +0300

    hw 01 done

--- 01_Lesson/homework_01.py
+++ 01_Lesson/homework_01.py
@@ -5,3 +5,11 @@
 # n = 123 -> res = 6 (1 + 2 + 3)
 # n = 100 -> res = 1 (1 + 0 + 0)
 
+number = 123
+result = 0
+
+while number > 0:
+    part = int(number % 10)
+    result = result + part
+    number = int((number / 10))
+print (result)
