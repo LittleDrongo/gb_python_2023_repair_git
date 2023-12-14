70b118d8eb4834c4171a2054334c2825daaec5ba
Fomin Alexey <fomin.alexey@outlook.com>
2023-12-14 13:24:00 +0300

    smtp works

--- other/eml_smtp_sample.py
+++ other/eml_smtp_sample.py
@@ -1,21 +1,20 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 
 def perform_calculation(argument):
     # Ваш код для выполнения расчетов
     result = ("А вот и аргумент" + " " + argument)
     return result
 
-def send_email(to_address, subject, body, attachment_path=None):
+def send_email(to_address, subject, body):
     # Ваши настройки для отправки электронного письма
     smtp_server = 'smtp.mail.ru'
     smtp_port = 587
     smtp_username = 'python_pars'
     smtp_password = 'tLSHqB1uBe2F3u5Q5xGc'
     from_address = 'python_pars@mail.ru'
+   
 
     # Создание объекта MIMEMultipart
     message = MIMEMultipart()
@@ -25,15 +24,6 @@ def send_email(to_address, subject, body, attachment_path=None):
 
     # Добавление текста в тело письма
     message.attach(MIMEText(body, 'plain'))
-
-    # Добавление вложения, если указан путь к файлу
-    if attachment_path:
-        attachment = open(attachment_path, 'rb')
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= %s" % attachment_path)
-        message.attach(part)
 
     # Создание объекта SMTP и отправка письма
     with smtplib.SMTP(smtp_server, smtp_port) as server:
@@ -48,10 +38,9 @@ def process_email(email_content):
     # Выполняем расчеты
     result = perform_calculation(argument)
 
-    # Отправляем результат обратно с вложением (замените на путь к вашему файлу)
-    attachment_path = '/eml_test.csv'
+    # Отправляем результат обратно
     sender_email = 'fomin.alexey@outlook.com'  # Замените на адрес отправителя
-    send_email(sender_email, 'Calculation Result', f'The result is: {result}', attachment_path)
+    send_email(sender_email, 'Calculation Result', f'The result is: {result}')
 
 # Пример использования:
 # if __name__ == "__main__":
